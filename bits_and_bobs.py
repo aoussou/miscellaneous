79fdf106019c29710234c3aa84caf9cbafe438e3
john <john@talisol.com>
2020-09-16 19:50:34 +0900

    save dict

--- bits_and_bobs.py
+++ bits_and_bobs.py
@@ -1,5 +1,11 @@
+import os
 ###############################################################################
 def create_dir(path):
     if not os.path.isdir(path):
         os.makedirs(path)
 ###############################################################################   
+dict_ = {}
+import json
+with open(os.path.join('name.json'), 'w') as fp:
+    json.dump(dict_, fp)
+fp.close()
