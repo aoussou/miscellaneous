f4cca561d46d8d299405429e6132f050ebb0cfe6
aoussou <aoussou@talisol.com>
2022-03-06 16:06:38 +0900

    read lines of txt file

--- bits_and_bobs.py
+++ bits_and_bobs.py
@@ -3,9 +3,20 @@ import os
 def create_dir(path):
     if not os.path.isdir(path):
         os.makedirs(path)
+        
 ###############################################################################   
+# SAVE A DICTIONARY
 dict_ = {}
 import json
 with open(os.path.join('name.json'), 'w') as fp:
     json.dump(dict_, fp)
 fp.close()
+
+###############################################################################   
+# GET LINES OF A TEXT FILE
+def readText(file_path):
+    with open(file_path) as f:
+        lines = f.readlines()
+    f.close()
+    
+    return lines
